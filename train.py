fa6c2f61432e5c9a1cc8ae9a69706a00a403b052
karim Ayman <kareem200404@gmail.com>
2025-10-15 23:42:20 +0300

    ran random-forest on data

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 import json
 import seaborn as sns
@@ -9,7 +9,7 @@ df = pd.read_csv('data/data_raw.csv')
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 # Train model (ensure you change this for other branches)
-model = LogisticRegression(random_state=42)
+model = RandomForestClassifier(random_state=42)
 model.fit(X, y)
 preds = model.predict(X)
 # Save metrics
